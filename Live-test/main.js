301113d94929ed30390001adb987e1234d17b4ae
partho1999 <sarker624452@gmail.com>
2023-07-04 23:22:18 +0600

    version-1.1.1

---
+++ Live-test/main.js
@@ -0,0 +1,25 @@
+
+// defining vowels
+const vowels = ["a", "e", "i", "o", "u"]
+
+function countVowel(str) {
+    // initialize count
+    let count = 0;
+
+    // loop through string to test if each character is a vowel
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    }
+
+    // return number of vowels
+    return count
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = countVowel(string);
+
+console.log(`there are ${result} vowel in this string`);
