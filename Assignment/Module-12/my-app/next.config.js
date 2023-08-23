f12d2655a72dd60f43c9b52a9af1b5b57ac08508
partho1999 <sarker624452@gmail.com>
2023-08-23 14:35:02 +0600

    version-3.3.3

--- Assignment/Module-12/my-app/next.config.js
+++ Assignment/Module-12/my-app/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build',
+    async  headers(){
+        return [
+            {
+                source:"/:path*",
+                headers:[
+                    {
+                        key:"X-Frame-Options",
+                        value:"DENY"
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
