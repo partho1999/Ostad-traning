3ea33df02cb0da81d970f721f2e63f73d2f1dcf5
partho1999 <sarker624452@gmail.com>
2023-08-29 22:37:04 +0600

    version-3.3.4

--- Assignment/Module-13/my-app/app/api/redirect/route.js
+++ Assignment/Module-13/my-app/app/api/redirect/route.js
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
  
 
 
 // Given an incoming request...
 
 export async function GET(req, res){
-    const loginUrl = new URL('/cookie', request.url)
+    
     // And redirect to the new URL
-    return NextResponse.redirect(loginUrl)
+    return NextResponse.redirect(new URL('/', req.nextUrl))
 }
--- Assignment/Module-13/my-app/app/lib/cookieAPI.js
+++ Assignment/Module-13/my-app/app/lib/cookieAPI.js
@@ -1,5 +1,5 @@
 export default async function getCookie(){
-    const res = await fetch('http://localhost:3000/api/cookie');
+    const res = await fetch('/api/cookie');
 
     if(!res.status===200){
         throw new Error('Network response was not ok');
