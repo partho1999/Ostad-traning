4f2ab723faab62c382361f3dcd9bd7ce07ba9d4c
partho1999 <sarker624452@gmail.com>
2023-09-30 22:44:40 +0600

    version-4.4.5

---
+++ Assignment/Module-16/my-app/src/components/AllService.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+async function getData(){
+  const res = await fetch(process.env.BASE_URL+'api/AllService');
+  if(!res.ok){
+    throw new Error('AllService calling fail');
+  }
+  return res.json();
+
+}
+
+async function AllService() {
+  const data = await getData();
+  return (
+    <>
+      <section>
+        <div className="py-20 bg-gray-50 radius-for-skewed">
+          <div className="container px-4 mx-auto">
+            <div className="mb-16 flex flex-wrap justify-center md:justify-between items-center">
+              <div className="text-center lg:text-left">
+                <span className="text-green-600 font-bold">
+                  Dolor sit amet consectutar
+                </span>
+                <h2 className="text-4xl lg:text-5xl font-bold font-heading">
+                  All Service
+                </h2>
+              </div>
+              <a
+                className="hidden md:inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-green-600 hover:bg-green-700 text-gray-50 font-bold leading-loose transition duration-200"
+                href="#"
+              >
+                View More Projects
+              </a>
+            </div>
+            <div className="flex flex-wrap -mx-4 mb-4">
+              {
+                data.map((item, index)=>{
+                  return(
+                    <div key={index} className="relative mb-4 w-full md:w-1/2 lg:w-1/3 px-4">
+                      <div className="relative h-80 mb-5 mx-auto rounded-lg">
+                          <img
+                            className="h-80 w-full relative h-full rounded-lg object-cover"
+                            src={item.image1}
+                            alt=""
+                          />
+                          <div className="absolute inset-0 bg-gray-900 opacity-75 rounded-lg" />
+                          <div className="absolute inset-0 p-6 flex flex-col items-start">
+                            <span className="text-gray-400">2021</span>
+                            <p className="mb-auto text-xl lg:text-2xl text-white font-bold">
+                              {item.des}
+                            </p>
+                          </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+              
+              
+              
+              
+            </div>
+            <div className="text-center">
+              <a
+                className="md:hidden inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-green-600 hover:bg-green-700 text-gray-50 font-bold leading-loose transition duration-200"
+                href="#"
+              >
+                View More Projects
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default AllService;
